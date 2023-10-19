7de3f111aec948f3a9c5f56debfffa1c0216ebe2
Анастасия Штомпель <anastasiastompel@MacBook-Pro-Anastasia.local>
2023-10-19 11:55:24 +0300

    fix select_genes_from_gbk_to_fasta function

--- bio_processor/bio_files_processor.py
+++ bio_processor/bio_files_processor.py
@@ -1,11 +1,10 @@
 import os
+from typing import TextIO, Optional
 
 AMINOACIDS = {'G', 'A', 'V', 'I', 'L', 'P',
               'S', 'T', 'C', 'M', 'N', 'Q', 
               'F', 'W', 'Y', 'D', 'E', 'K', 
               'R', 'H'}
-
-
 
 def dict_to_fasta(fasta_dict, output_fasta=None):   
     if output_fasta is None:
@@ -34,7 +33,7 @@ def convert_multiline_fasta_to_oneline(input_fasta, output_fasta = None):
         return dict_to_fasta(fasta_dict, output_fasta)
 
     
-def filter_gbk(file):
+def filter_gbk(file: str) -> list:
     with open(file) as lines:
         seq_list = []
         gene = None
@@ -42,9 +41,9 @@ def filter_gbk(file):
         for line in lines:
             line = line.strip()
             if line.startswith('/gene'):
-                gene = line.strip('/gene="')
+                gene = line.strip()
                 gene = '>'+ gene
-                seq_list.append(gene)
+                seq_list.append(gene.replace('>/gene=', '>gene='))
             if line.startswith('/translation') or set(line).issubset(AMINOACIDS):
                 if gene is None:
                     continue
@@ -54,43 +53,40 @@ def filter_gbk(file):
     return seq_list
 
 
-def get_gene_names(seq_list):
+def get_gene_names(seq_list: list) -> list:
     names = []
-    for seq in my_list:
-        if seq.startswith('>'):
-            names.append(seq.strip('>'))
+    for seq in seq_list:
+        if '>gene=' in seq:
+            names.append(seq.strip('>gene="'))
     return names
 
 
-def find_range_genes(seq_list, names_seq, gene, n_before, n_after):
+def find_range_genes(seq_list: list, names: list, gene: str, n_before: int, n_after: int) -> list:
     upper_section = []
     lower_section = []
-    left = names_seq.index(gene) - n_before   
-    right = names_seq.index(gene) + n_after   
-    upper = seq_list.index('>'+ names_seq[left])
-    selected_gene = seq_list.index('>'+ gene)
-    lower = seq_list.index('>'+ names_seq[right])   
+    left = names.index(gene) - n_before   
+    right = names.index(gene) + n_after   
+    upper = seq_list.index('>gene=\"'+names[left]+'"')
+    selected_gene = seq_list.index('>gene="'+gene+'"')
+    lower = seq_list.index('>gene=\"'+names[right]+'"')   
     for i in range(upper, selected_gene):
-        upper_section.append(my_list[i])
+        upper_section.append(seq_list[i])
     for i in range(lower, selected_gene, -1):
-        lower_section.append(my_list[i])       
+        lower_section.append(seq_list[i])       
     sum_list_genes =  upper_section + lower_section
     return sum_list_genes
 
 
-def write_to_fasta(gene_list, output_fasta):
-    with open(output_fasta, mode='w') as output:
-        for line in gene_list:
+def write_to_fasta(sum_list_genes: list, output_fasta: str) -> None:
+    with open (output_fasta, mode = 'w') as output:
+        for line in sum_list_genes:
             output.write(line + '\n')
+        
 
-
-def select_genes_from_gbk_to_fasta(input_gbk, genes, output_fasta = None, n_before = 1, n_after = 1):
+def select_genes_from_gbk_to_fasta(input_gbk: str, genes: str, output_fasta: str = 'file.fasta' , n_before: int = 1, n_after: int = 1) -> str:
+    output_list = [] 
     filter_gene_list = filter_gbk(input_gbk)
     gene_of_interest_list = get_gene_names(filter_gene_list)
     output_list = find_range_genes(filter_gene_list, gene_of_interest_list, genes, n_before, n_after)
-    if output_fasta is None:
-        output_fasta = 'file.fasta' 
-    if not output_fasta.endswith('.fasta'):
-        output_fasta += '.fasta'
     write_to_fasta(output_list, output_fasta)
     return f'Fasta file was written in {os.path.join(os. getcwd(), output_fasta)}.'
